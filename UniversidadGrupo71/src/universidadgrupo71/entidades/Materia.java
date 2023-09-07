6401c68ea6a200d77e7e98114015e6e2fec47108
Joel Brondo <joelbrondo@gmail.com>
2023-09-07 20:13:19 -0300

    proyecto

---
+++ UniversidadGrupo71/src/universidadgrupo71/entidades/Materia.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package universidadgrupo71.entidades;
+
+public class Materia {
+    
+    private int id;
+    private String nombre;
+    private int anio;
+    private boolean estado;
+    
+    public Materia(){};
+
+    public Materia(int id, String nombre, int anio, boolean estado) {
+        this.id = id;
+        this.nombre = nombre;
+        this.anio = anio;
+        this.estado = estado;
+    }
+
+    public Materia(String nombre, int anio, boolean estado) {
+        this.nombre = nombre;
+        this.anio = anio;
+        this.estado = estado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return this.nombre + " " + this.anio;
+    }
+    
+    
+
+}
