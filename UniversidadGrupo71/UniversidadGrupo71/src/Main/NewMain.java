db78d6c37dfc0de2264102282defb951e35fde4f
RodriLang <Rodrigolang90@gmail.com>
2023-09-22 20:59:39 -0300

    resuelto

--- UniversidadGrupo71/UniversidadGrupo71/src/Main/NewMain.java
+++ UniversidadGrupo71/UniversidadGrupo71/src/Main/NewMain.java
@@ -3,10 +3,13 @@ package Main;
 
 import AccesoADatos.AlumnoData;
 import AccesoADatos.Conexion;
+import AccesoADatos.InscripcionData;
+import AccesoADatos.MateriaData;
 import java.sql.Connection;
 import java.time.LocalDate;
 import java.time.Month;
 import universidadgrupo71.entidades.Alumno;
+import universidadgrupo71.entidades.Inscripcion;
 
 
 public class NewMain {
@@ -17,8 +20,19 @@ public class NewMain {
         Connection conexion=Conexion.getConexion();
         
         AlumnoData alu= new AlumnoData();
+        MateriaData mat = new MateriaData();
         //alu.guardarAlumno(new Alumno (34768990,"Sosa","Laura",LocalDate.of(1988, Month.MARCH, 15),true));
         //alu.modificarAlumno(new Alumno (6,34768990,"Sosa","Maria Laura",LocalDate.of(1988, Month.MARCH, 15),true));
-    alu.eliminarAlumno(6);
+        InscripcionData ins = new InscripcionData(mat, alu);
+        Inscripcion insc = new Inscripcion(2, alu.buscarAlumnoId(1), mat.buscarMateria(1));
+        
+        //ins.guardarInscripcion(insc);
+        
+        
+        System.out.println(mat.listarMaterias());
+        
+        System.out.println("");
+        System.out.println("Inactivas");
+        System.out.println(mat.listarMateriasInactivas());
 }
 }
