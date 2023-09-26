5f49ab7b8efa55de3987101bc78cad773dee721e
Usuario <Usuario@DESKTOP-5KTL61N>
2023-09-25 23:27:53 -0300

    25/09

--- UniversidadGrupo71/UniversidadGrupo71/src/Entidades/Usuario.java
+++ UniversidadGrupo71/UniversidadGrupo71/src/Entidades/Usuario.java
@@ -12,6 +12,8 @@
  */
 package Entidades;
 
+import java.time.LocalDate;
+
 public class Usuario {
 
     private int id;
@@ -19,37 +21,114 @@ public class Usuario {
     private String nombre;
     private String apellido;
     private int dni;
+    private LocalDate fechaNac;
     private String email;
     private String usuario;
     private char[] clave;
-    private int acceso;
     private boolean activo;
 
     public Usuario() {
     }
 
-    public Usuario(String nombre, String apellido, int dni, String email, 
-            String usuario, char[] clave, int acceso, boolean activo) {
+    public Usuario(String nombre, String apellido, int dni, LocalDate fechaNac, String email, String usuario, char[] clave, boolean activo) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.dni = dni;
+        this.fechaNac = fechaNac;
         this.email = email;
         this.usuario = usuario;
         this.clave = clave;
-        this.acceso = acceso;
         this.activo = activo;
     }
 
-    public Usuario(int id, String nombre, String apellido, int dni, String email,
-            String usuario, char[] clave, int acceso, boolean activo) {
+    public Usuario(int id, String nombre, String apellido, int dni, LocalDate fechaNac, String email, String usuario, char[] clave, boolean activo) {
         this.id = id;
         this.nombre = nombre;
         this.apellido = apellido;
         this.dni = dni;
+        this.fechaNac = fechaNac;
         this.email = email;
         this.usuario = usuario;
         this.clave = clave;
-        this.acceso = acceso;
         this.activo = activo;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public LocalDate getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(LocalDate fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public char[] getClave() {
+        return clave;
+    }
+
+    public void setClave(char[] clave) {
+        this.clave = clave;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    @Override
+    public String toString() {
+        return this.id+" "+this.nombre+" "+this.apellido+" "+this.dni+" "+this.getFechaNac()+" "+this.email+" "+this.usuario+" "+this.clave+" "+this.activo;
+    }
+
+   
 }
